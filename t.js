d94bdfea81b4ba4cd503e606c87964816928ec46
husone <huynhson1203@gmail.com>
2023-08-28 00:43:06 +0700

    delete file

--- t.js
+++
@@ -1,7 +0,0 @@
-const cheerio = require('cheerio');
-const fs = require('fs');
-const { unabbreviateNumber } = require('js-abbreviation-number');
-
-let $ = cheerio.load(fs.readFileSync('tiktok.html'));
-
-console.log($('h2[data-e2e="user-subtitle"]').text() == '');
